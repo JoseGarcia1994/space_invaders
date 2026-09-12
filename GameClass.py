a24fcbbe7539a941b89e5262b13bf7def5b5e896
Jose Garcia <garciacolin@hotmail.com>
2026-09-12 13:04:03 -0600

    feat: implement main menu and high score screen

--- GameClass.py
+++ GameClass.py
@@ -6,15 +6,27 @@ import turtle
 BULLET_IMAGE = pygame.image.load(os.path.join('img','bullet_image.png'))
 
 class Game:
-    def __init__(self, font, FPS, count, window, lives, screen_width, screen_height, bullets = 0, clock = pygame.time.Clock()):
-        self.font = font
-        self.window = window
+    def __init__(self,font, FPS, lives, window, screen_width, screen_height, bullets = 0, clock = pygame.time.Clock() ):
+        self.font =font
         self.HEIGHT = screen_height
         self.WIDTH = screen_width
         self.bullets = bullets
         self.bullet_image = BULLET_IMAGE
         self.lives = lives
         self.FPS = FPS
+        self.FPS = FPS
+        self.lives = lives
+        self.level  = 1
+        self.count = 0
+        self.window = window
+        self.clock = clock
+        self.bullets = bullets #aqui
+        self.bullet_img = BULLET_IMAGE
+        registros = self.leer_registros("puntajes.txt")
+        if len(registros) > 0:
+            self.jugador, self.max_pun = registros[0] 
+        else:
+            self.max_pun = 0 
         
 
     def escape(self):
@@ -41,6 +53,9 @@ class Game:
 
     def reload_bullets(self, bullets):
         self.bullets = bullets
+
+    def reload_bullet(self,bullet):
+        self.bullets = bullet
     
     def draw_HUD(self):
         offset = 0
